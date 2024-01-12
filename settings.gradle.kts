26233e3cbb6ac0951842f8bbb2949f9cab88a6f8
Inaki Villar <inaki.seri@gmail.com>
2024-01-12 11:54:18 -0800

    adding ge

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,23 @@ pluginManagement {
     }
 }
 
+plugins {
+ id("com.gradle.enterprise") version "3.16.1"
+
+}
+
+gradleEnterprise {
+    server = "http://ge.solutions-team.gradle.com"
+    allowUntrustedServer = true
+    buildScan {
+        publishAlways()
+        capture {
+            isTaskInputFiles = true
+        }
+        isUploadInBackground = System.getenv("CI") == null
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
